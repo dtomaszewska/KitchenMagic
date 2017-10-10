9d0513b06caa4335219884ec21ae93bfbe926396
Dominika Tomaszewska <ayfer@o2.pl>
2017-10-10 15:47:43 +0200

    Some little styling

--- KitchenMagic.Wpf/Converters/IsCategoryToFontStyleConverter.cs
+++ KitchenMagic.Wpf/Converters/IsCategoryToFontStyleConverter.cs
@@ -6,15 +6,15 @@ using System.Windows.Data;
 
 namespace KitchenMagic.Wpf.Converters
 {
-	class IsCategoryToFontWeightConverter : IValueConverter
+	class IsCategoryToFontStyleConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType,
 							object parameter, CultureInfo culture)
 		{
 			if (value is CategoryPO)
-				return FontWeights.Bold;
+				return (Style)Application.Current.Resources["BoldLabelStyle"];
 
-			return FontWeights.Normal;
+			return (Style)Application.Current.Resources["LabelStyle"];
 		}
 
 		public object ConvertBack(object value, Type targetType,
